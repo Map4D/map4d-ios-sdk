42158eee02f79e436c7f82ed228295a3317b25c8
tantv <tanht268@gmail.com>
2019-01-15 17:32:50 +0700

    add demo map4dsdk example

---
+++ examples/ios-sdk-examples/ios-sdk-examples/Views/Home/polylines/PolylineViewController.swift
@@ -0,0 +1,121 @@
+//
+//  PolylineViewController.swift
+//  ios-sdk-examples
+//
+//  Created by tantv on 1/9/19.
+//  Copyright © 2019 tantv. All rights reserved.
+//
+
+import UIKit
+import map4dsdk
+
+class PolylineViewController: UIViewController {
+
+  @IBOutlet weak var labelHide: UIButton!
+  @IBOutlet weak var tvNotice: UILabel!
+  @IBOutlet weak var map4d: MFMapView!
+  var polyline: MFPolyline?
+  var isAddPolyline = false
+  var pathPolyline = MFMutablePath.init()
+  var sizePath = 4;
+  var isInitPolyline = false
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.navigationItem.title = "Polyline"
+    initData()
+    addPolyline()
+    map4d.delegate = self
+  }
+  func initData(){
+    if (!isInitPolyline){
+      pathPolyline.removeAllCoordinates()
+      pathPolyline.add(CLLocationCoordinate2DMake(16.061583,108.234463))
+      pathPolyline.add(CLLocationCoordinate2DMake(16.038148,108.243031))
+      pathPolyline.add(CLLocationCoordinate2DMake(16.032951,108.224449))
+      pathPolyline.add(CLLocationCoordinate2DMake(16.031512,108.208902))
+      sizePath = 4
+      isInitPolyline = true
+    }
+  }
+  func addPolyline(){
+    if (sizePath < 4){
+      isInitPolyline = false
+      if (isAddPolyline){
+        polyline?.map = nil
+        isAddPolyline = false
+      }
+      initData()
+    }
+    if (!isAddPolyline){
+      // Move Camera To Polyline Position Before Draw
+      let bounds = MFCoordinateBounds().includingPath(pathPolyline)
+      let cameraUpdate = MFCameraUpdate.fit((bounds as! MFCoordinateBounds))
+      map4d.moveCamera(cameraUpdate)
+      
+      // Init Polyline
+      polyline = MFPolyline.init(pathPolyline, closedPolyline: true)
+      polyline?.color = .blue
+      polyline?.width = 4
+      polyline?.map = map4d
+      
+      isAddPolyline = true
+    }
+  }
+  
+  func removePolyline(){
+    polyline?.map = nil
+    isAddPolyline = false
+  }
+  
+  
+  @IBAction func addPolyline(_ sender: Any) {
+    addPolyline()
+  }
+  
+  @IBAction func removePolyline(_ sender: Any) {
+    removePolyline()
+  }
+  
+  @IBAction func updatePolyline(_ sender: Any) {
+    if (!isAddPolyline){
+      addPolyline()
+    }
+    polyline?.color = .red
+    polyline?.width = 10
+    // Remove last coordinate
+    if (sizePath > 1){
+      sizePath-=1;
+      pathPolyline.removeLastCoordinate()
+    } else {
+      isInitPolyline = false
+      initData()
+    }
+    polyline?.path = pathPolyline
+  }
+  
+  
+  @IBAction func hideAndShowPolyline(_ sender: Any) {
+    polyline?.visible = !(polyline?.visible)!
+    if ((polyline?.visible)!){
+      labelHide.setTitle("hide", for: .normal)
+    } else {
+      labelHide.setTitle("show", for: .normal)
+    }
+  }
+  
+}
+
+
+extension PolylineViewController: MFMapViewDelegate {
+  
+  func mapview(_ mapView: MFMapView!, didTap polyline: MFPolyline!) {
+    tvNotice.text = "Polyline clicked Id= " + String(polyline.id)
+  }
+  func mapView(_ mapView: MFMapView!, idleAt position: MFCameraPosition!) {
+    NSLog("lat lng:  (" + String(position.target.latitude) + " , " + String(position.target.longitude) + ") ")
+    NSLog("zoom :  " + String(position.zoom))
+  }
+  func mapView(_ mapView: MFMapView!, didTapAt coordinate: CLLocationCoordinate2D) {
+    tvNotice.text = "Map clicked: " + String(coordinate.latitude) + " , " + String(coordinate.longitude)
+  }
+}
---
+++ examples/ios-sdk-examples/ios-sdk-examplesTests/ios_sdk_examplesTests.swift
@@ -0,0 +1,34 @@
+//
+//  ios_sdk_examplesTests.swift
+//  ios-sdk-examplesTests
+//
+//  Created by tantv on 1/5/19.
+//  Copyright © 2019 tantv. All rights reserved.
+//
+
+import XCTest
+@testable import ios_sdk_examples
+
+class ios_sdk_examplesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
